909af1b72da96e71b25945d09bf86a3e4d2f741a
Wiam <wiamnasr@gmail.com>
2021-12-06 19:43:52 +0000

    animated the cart button on add item with useEffect and useState and setting timeout

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,5 +1,5 @@
 // Using the Context in my components, first importing the React hook
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 // Second importing the Context itself
 import CartContext from "../../../store/cart-context";
 
@@ -10,6 +10,9 @@ import CartIcon from "../../Cart/CartIcon/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  // this will control whether or not the button should be animated
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   /* 
     Third, calling useContext, passing in the CartContext to get access to the CartContext, managed by the closest provider (CartProvider used in the App component) inside of the HeaderCartButton component
     Now the HeaderCartButton will be re-evaluated by React whenever the cartCtx changes (Context)
@@ -28,13 +31,37 @@ const HeaderCartButton = (props) => {
 
   */
 
-  const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
+  // using object destructuring to pull out the items from the cartCtx so I can just refer to them later, so that not the entire conteXt but only the items array is a dependency for this effect function
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currNum, item) => {
     // My CartItems objects will have an amount field which stores the number of items per item type
     return currNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    // setting a timer to remove the class so when the user clicks the button again the animation plays again not just the first time the button is clicked
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // cleanup function, if many timers were added on top of each other, i want to clear the old timer and make sure that a new timer is set
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
